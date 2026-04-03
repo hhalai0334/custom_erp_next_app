4848b33b66de14d45dd6c81567ed7977fb1f1d86
Hussain <h.halai0334@gmail.com>
2026-04-03 09:17:05 +0000

    Add User Setting

---
+++ warehouse_management/warehouse_management/doctype/usersettings/usersettings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, h.halai0334@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("UserSettings", {
+// 	refresh(frm) {
+
+// 	},
+// });
