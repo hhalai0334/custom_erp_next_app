b9110480ed4173520f96487f4d49052a0468a851
Hussain Murtaza <h.halai0334@gmail.com>
2026-05-12 18:49:28 +0500

    Implement warehouse pricing validation
    Add validation to ensure only one pricing option can be selected.

--- warehouse_management/overrides/warehouse.py
+++ warehouse_management/overrides/warehouse.py
@@ -2,7 +2,13 @@ from erpnext.stock.doctype.warehouse.warehouse import Warehouse
 import frappe
 
 class CustomWarehouse(Warehouse):
+    def validate_warehouse_price_flags(doc, method=None):
+    if doc.custom_show_warehouse_pricing and doc.custom_show_wholesale_price:
+        frappe.throw(
+            _("Only one of Warehouse Pricing or Wholesale Price can be selected.")
+        )
 
+    
     def autoname(self):
         if not self.custom_warehouse_code:
             frappe.throw("Custom Warehouse Code is required")
